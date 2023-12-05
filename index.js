95bce5b8d1bfd7663c87fd8bafca672df83c6462
GhislainFaure <ghislainfaure06@gmail.com>
2023-12-05 22:35:43 +0100

    factorisation dans le header et dans le router

--- index.js
+++ index.js
@@ -4,15 +4,38 @@ const ejs = require("ejs");
 const port = 3000;
 const app = express();
 const router = require("./router");
+const gamesJson = require('./games.json');
 
 
-
+let compteurVisites = 0;
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
+// Je définie une propriété de l'objet locals dans TOUTES les vues
+// Toutes les vues pourront manipuler une variable locals.games qui contiendra
+// un tableau d'objet, la liste des jeux
+app.locals.games = gamesJson;
+
 // les ressources publiques sont rangées dans le dossier public
 app.use(express.static("public"));
-
+// MW compteur de visites
+app.use((req, res, next) => {
+  compteurVisites++;
+  console.log(`Le nombre de visiteurs est de ${compteurVisites}`)
+  next();
+}) 
+// MW de journalisation
+app.use((req, res, next) => {
+  let today = new Date()
+  console.log(today);
+  next();
+  
+})
 app.use(router);
+// MW pour gérer la page 404
+app.use((req, res) => {
+  res.render('404')
+})
 
 app.listen(port, () => {
   console.log(`app is listening to http://localhost:${port}`);
--- router.js
+++ router.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const gamesJson = require('./games.json');
 
 
 router.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs",);
 });
-router.get("/game/diceRoller", (req, res) => {
-  res.render("diceRoller", {cssSuplementaire: '/css/diceRoller.css'});
+
+// on filtre sur le tableau pour recuperer le jeu 
+
+
+router.get("/game/:game", (req, res) => {
+const gameSelected = req.params.game;
+
+const foundGame = gamesJson.find((game) => game.name === gameSelected )
+console.log(foundGame);
+if (foundGame){
+  res.render(foundGame.name, {cssSuplementaire: foundGame.cssFile})
+}
+else {
+  next();
+}
+
+
+// router.get("/game/diceRoller", (req, res) => {
+//   res.render("diceRoller", {cssSuplementaire: '/css/diceRoller.css'});
+// });
+
 });
-router.get("/game/fourchette", (req, res) => {
-  res.render("fourchette");
-});
+
+
+
+
+
+
+
 
 module.exports = router;
